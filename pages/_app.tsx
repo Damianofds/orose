b3a1c8f0483e6aa268bab59263189ef9bfef86e2
Damianofds <damianofds@gmail.com>
2022-11-24 22:50:56 +0100

    initial commit, first landing page draft

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { MetaMaskProvider } from "metamask-react";
+import MetaMaskCard from '../components/connectorCards/MetaMaskCard';
+import ProviderExample from '../components/ProviderExample';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MetaMaskProvider>
+      <ChakraProvider>
+      {/* <ProviderExample />
+      <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
+        <MetaMaskCard />
+      </div> */}
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </MetaMaskProvider>
+  )
 }
