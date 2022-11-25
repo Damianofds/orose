1472ccf78d75e4fc59a5c23fbf9b0c5b9caa293b
Damianofds <damianofds@gmail.com>
2022-11-25 20:51:27 +0100

    adjusted user journey layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -177,29 +177,29 @@ const App = () => {
 
                   </div>
                   <div id="section3" key="section3" className="section">
-                    <SimpleGrid columns={2} spacing={10}>
+                    <SimpleGrid minChildWidth='150px' spacing={10}>
                       <ScaleFade initialScale={0.9} in={isOpen}>
                         <Box height='150px' backgroundColor={'#c27a74'} padding={'10px'}>
-                          <Heading as='h2' fontSize={'2xl'}>Learn the basics of Web3 and Digital Assets</Heading>
-                          <Text fontSize={'xl'}>Open a crypto wallet and put some coins in</Text> 
+                          <Heading as='h2' fontSize={'xl'}>Learn the basics of Web3 and Digital Assets</Heading>
+                          <Text fontSize={['sm']}>👛 Open a crypto wallet and put some coins in 🪙</Text> 
                         </Box>
                       </ScaleFade>
                       <ScaleFade initialScale={0.9} in={isOpen}>
                         <Box height='150px' backgroundColor={'#c27a74'} padding={'10px'}>
-                          <Heading as='h2' fontSize={'2xl'}>Mint the NFT for 0.1 ETH</Heading>
-                          <Text fontSize={'xl'}>Get the ownership of the digital collectible</Text> 
+                          <Heading as='h2' fontSize={'xl'}>Mint the NFT for 0.1 ETH</Heading>
+                          <Text fontSize={'sm'}>Get the ownership of the digital collectible 📿</Text> 
                         </Box>
                       </ScaleFade>
                       <ScaleFade initialScale={0.9} in={isOpen}>
                         <Box height='150px' backgroundColor={'#c27a74'} padding={'10px'}>
-                          <Heading as='h2' fontSize={'2xl'}>Access to the NFT lounge</Heading>
-                          <Text fontSize={'xl'}>You have now claiming rights for physical jewel ✌️</Text> 
+                          <Heading as='h2' fontSize={'xl'}>Access to the NFT lounge</Heading>
+                          <Text fontSize={'sm'}>You have now claiming rights for physical jewel ✌️</Text> 
                         </Box>
                       </ScaleFade>
                       <ScaleFade initialScale={0.9} in={isOpen}>
                         <Box height='150px' backgroundColor={'#c27a74'} padding={'10px'}>
-                          <Heading as='h2' fontSize={'2xl'}>Hodl or trader?</Heading>
-                          <Text fontSize={'xl'}>Do you want to hold it and sell the claiming rights later? 😏</Text> 
+                          <Heading as='h2' fontSize={'xl'}>Hodl or trader?</Heading>
+                          <Text fontSize={'sm'}>Do you want to hold it and sell the claiming rights later? 😏</Text> 
                         </Box>
                       </ScaleFade>
                     </SimpleGrid>
